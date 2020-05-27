96f8281df70e88963b180a73d41ed78c2fca8400
luke <lddeitz@gmail.com>
2020-05-27 12:43:10 -0500

    adds 3 products to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Dual Headphone Adapter", price: 5.88, image_url: " https://images-na.ssl-images-amazon.com/images/I/51oPPdbyZiL._AC_SY355_.jpg", description: "With this headphone jack adapter, you can control music (volume up/ volume down/ pause/ next track), or answer telephone call as you wish; You can charge your iPhone when enjoying your music/ videos/ movies at the same time, and you don't need to worry about the battery of your iPhone runs out")
+
+Product.create(name: "Animal Charging Cable Protector", price: 4.49, image_url: "https://www.claires.com/dw/image/v2/BBTK_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4feeb122/images/hi-res/01523_1.jpg?sw=2000&sh=2000&sm=fit", description: "Decoration and protection for your cables. Unicorn accessory help reinforce and protect your charger from damage.")
+
+Product.create(name: "Monkey Lamp", price: 129.99, image_url: "https://images-na.ssl-images-amazon.com/images/I/510erxf9b1L._AC_SL1143_.jpg", description: "Resin monkey lamp to light your bedroom, living room, or office space.")
