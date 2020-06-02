5f62e0ada5f82d6f7fc3b0b5d92badb98be3b5d4
luke <lddeitz@gmail.com>
2020-06-02 14:59:05 -0500

    adds working query parameters

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,78 @@
 class Api::ProductsController < ApplicationController
-def all_products_action
+# require 'http'
+
+# params = {params[:id]}
+
+# response = HTTP.get('localhost:3000/api/products', params)
+
+  def all_products_action
   @products = Product.all
   render 'all_products.json.jb'
 end
+
+# def product1_action
+#   @product = Product.find_by(id: 1)
+#   render 'product.json.jb' 
+# end
+
+def product1_action
+    @name = params["name"]
+    render 'product.json.jb' 
+end
+
+def product2_action
+  @product = Product.find_by(id: 2)
+  render 'product.json.jb' 
+end
+
+def product3_action
+  @product = Product.find_by(id: 3)
+  render 'product.json.jb'  
+end
+
+def all_products_actionquery
+  @product = Product.find_by(params["name"])
+  render 'newquery.json.jb' 
+end 
+
+# def index
+#   @product = Product.find_by(id: params[:id])
+#   render 'index.json.jb'
+# end
+
+def create
+  @product = Product.new(
+    name: params[:name],
+    price: params[:price],
+    description: params[:description],
+    image_url: params[:image_url]
+  )
+  @product.save
+  render 'create.json.jb'
+end
+
+def update
+#find the product
+  @product = Product.find_by(id: params[:id])
+#update attributes
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+#save the product
+  @product.save
+#render the view
+  render 'create.json.jb'
+end
+
+def destroy
+  @product = Product.find_by(id: params[:id])
+  @product.destroy
+  render json: {message: "Successfully destroyed!"}
+end
+
+def index
+  
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,26 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/shop" => "products#all_products_action"
+    # get "/shop" => "products#all_products_action"
+
+    # get "/product-1" => "products#product1_action"
+
+    # get "/product-2" => "products#product2_action"
+
+    # get "/product-3" => "products#product3_action"
+
+    #Query Parameter
+    # get "/query-parameter" => "products#all_products_actionquery"
+
+    #REST
+
+    get "/products" => "products#index"
+
+    post "products" => "products#create"
+
+    patch "products/:id" => "products#update"
+
+    delete "products/:id" => "products#destroy"
+       
   end
 end
